5d8fb9905099c99a35dd2c8f7e93ce0017608292
Kylan <kpurdon87@gmail.com>
2023-06-17 19:44:17 -0400

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {return a + b;
+}
+function subtract(a, b) {return a - b;
+}
+function multiply(a, b) {return a * b;
+}
+function divide(a, b) {return a / b;
+}
+let number =5;
+function increment(number) {
+return ++number;
+return number;
+return number++;
+}
+let n =6;
+function decrement(n) {
+return --n;
+return n;
+return n--;
+}
+let y = 20.22
+let Radix = 10
+function makeInt(y) {
+    parseInt(y, Radix)
+    return y;
+}
+function preserveDecimal(y) {
+    parseFloat("y")
+    return y;
+}
+
+
+
